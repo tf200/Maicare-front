a8a7dda3b589e8f5a0372157e2eea9fcc042e2bf
Nouamane Elmkadem <nouamanlamkadmxd@gmail.com>
2024-01-29 17:34:05 +0100

    handle click and consume auth api

--- hooks/useSignIn.tsx
+++ hooks/useSignIn.tsx
@@ -1,5 +1,26 @@
-export default function useSignIn() {
-  return {
-    signIn: async (x: any) => {},
-  };
-}
+import axios from "axios";
+
+const useSignIn = async (
+  authData: {},
+  setIsLoading: any,
+  setAlertMessage: any
+) => {
+  setIsLoading(true);
+  setAlertMessage("");
+
+  await axios
+    .post(process.env.NEXT_PUBLIC_AUTH_API, authData)
+    .then((response) => {
+      console.log("Response:", response.data);
+      localStorage.setItem("access", response.data.access);
+      localStorage.setItem("refresh", response.data.refresh);
+      window.open("/dashboard/crm", "_self");
+    })
+    .catch((error) => {
+      console.log(error);
+      setIsLoading(false);
+      setAlertMessage("No active account found with the given credentials");
+    });
+};
+
+export default useSignIn;
