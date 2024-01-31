e08843d1a1257bc0189cda0a6251f55c6445f719
Nouamane Elmkadem <nouamanlamkadmxd@gmail.com>
2024-01-31 15:55:34 +0100

    added emergency relation options data for relation inputField

--- consts/index.ts
+++ consts/index.ts
@@ -9,7 +9,7 @@ export const DIAGNOSIS_SEVERITY_OPTIONS: SelectionOption[] = [
   { label: "Severe", value: "Severe" },
 ];
 
-export const DIAGNOSIS_RELATION_OPTIONS: SelectionOption[] = [
+export const EMERGENCY_RELATION_OPTIONS: SelectionOption[] = [
   { label: "Select Relation", value: "" },
   { label: "Relation 1", value: "Relation 1" },
   { label: "Relation 2", value: "Relation 2" },
