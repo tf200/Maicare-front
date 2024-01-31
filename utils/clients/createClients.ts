76d075885d5a88cbf36c13880e72063b5cb99b8b
saadazghour <saad.azghour@gmail.com>
2024-01-31 15:34:29 +0100

    feat: refactor useCreateClients to include onSuccess callback functionality

---
+++ utils/clients/createClients.ts
@@ -0,0 +1,24 @@
+import api from "@/utils/api";
+import { useMutation, useQueryClient } from "react-query";
+import { NewClientsRequest } from "@/types/clients/new-clients-request";
+
+export async function createClients(data: NewClientsRequest) {
+  const response = await api.post("/client/client_create/", data);
+  return response.data;
+}
+
+export const useCreateClients = (onSuccess?: () => void) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: createClients,
+
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries(["clients"]);
+      if (onSuccess) {
+        onSuccess(); // Call the provided onSuccess callback
+      }
+    },
+  });
+};
