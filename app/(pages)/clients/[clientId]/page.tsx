26382bcbde15b466293351f88c10e578a0bf128e
mellouki-atlasai <156778978+mellouki-atlasai@users.noreply.github.com>
2024-02-01 17:58:12 +0100

    add navigation to diagnosis and contacts list

--- app/(pages)/clients/[clientId]/page.tsx
+++ app/(pages)/clients/[clientId]/page.tsx
@@ -5,6 +5,7 @@ import ClientInformation from "@/components/clientDetails/ClientInformation";
 import MedicalRecordSummary from "@/components/clientDetails/MedicalRecordSummary";
 import EmergencyContactsSummary from "@/components/clientDetails/EmergyencyContactsSummary";
 import DocumentsSummary from "@/components/clientDetails/DocumentsSummary";
+import LinkButton from "@/components/buttons/LinkButton";
 
 type Props = {
   params: { clientId: string };
@@ -21,12 +22,30 @@ const ClientDetailsPage: FunctionComponent<Props> = ({
           <Panel title={"Client information"} containerClassName="px-7 py-4">
             <ClientInformation clientId={parseInt(clientId)} />
           </Panel>
-          <Panel title={"Emergency Contacts"} containerClassName="px-7 py-4">
+          <Panel
+            title={"Emergency Contacts"}
+            containerClassName="px-7 py-4"
+            sideActions={
+              <LinkButton
+                text={"Full Contacts List"}
+                href={`/clients/${clientId}/emergency`}
+              />
+            }
+          >
             <EmergencyContactsSummary clientId={parseInt(clientId)} />
           </Panel>
         </div>
         <div className="flex flex-col gap-9">
-          <Panel title={"Medical Record"} containerClassName="px-7 py-4">
+          <Panel
+            title={"Medical Record"}
+            containerClassName="px-7 py-4"
+            sideActions={
+              <LinkButton
+                text={"Full Diagnosis List"}
+                href={`/clients/${clientId}/diagnosis`}
+              />
+            }
+          >
             <MedicalRecordSummary clientId={parseInt(clientId)} />
           </Panel>
           <Panel title={"Reports & Documents"} containerClassName="px-7 py-4">
