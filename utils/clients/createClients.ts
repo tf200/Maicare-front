ca2e56f14e047efd38aed9beb5f85bf71beb2a65
saadazghour <saad.azghour@gmail.com>
2024-02-05 12:42:10 +0100

    feat(createClients): Add file upload in client Creation
    - Implement logic to handle non-file fields by appending them to `FormData`, ensuring all client data is sent in the request.

--- utils/clients/createClients.ts
+++ utils/clients/createClients.ts
@@ -3,7 +3,30 @@ import { useMutation, useQueryClient } from "react-query";
 import { NewClientsRequest } from "@/types/clients/new-clients-request";
 
 export async function createClients(data: NewClientsRequest) {
-  const response = await api.post("/client/client_create/", data);
+  const formData = new FormData();
+
+  formData.append("profile_picture", data.profile_picture);
+
+  delete data.profile_picture;
+
+  Object.keys(data).forEach((key) => {
+    if (key !== "profile_picture") {
+      formData.append(key, String(data[key]));
+    }
+  });
+
+  // Append other client data fields to formData
+  Object.keys(data).forEach((key) => {
+    formData.append(key, data[key].toString());
+  });
+
+  const response = await api.post("/client/client_create/", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Accept: "application/json",
+    },
+  });
+
   return response.data;
 }
 
@@ -11,7 +34,7 @@ export const useCreateClients = (onSuccess?: () => void) => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: createClients,
+    mutationFn: async (data: NewClientsRequest) => createClients(data),
 
     onSuccess: () => {
       // Invalidate and refetch
