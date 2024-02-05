dbffa08cef4204b1dcaf69658af12754e2368214
saadazghour <saad.azghour@gmail.com>
2024-02-05 12:33:16 +0100

    feat(clients-form): refactor client form with file upload and additional fields
    - Implement file upload functionality for profile pictures using Image component and CameraIcon for visual feedback.
    - Add new form fields (city, Zipcode, infix, streetname, street_number) to capture comprehensive client information.
    - Introduce image preview functionality to display selected profile picture before submission.
    - Address UI enhancements with LoadingCircle component for better user feedback during file upload.

--- components/forms/ClientsForm.tsx
+++ components/forms/ClientsForm.tsx
@@ -18,6 +18,7 @@ import RadioCustom from "../Checkboxes/RadioCustom";
 import Button from "../buttons/Button";
 import CheckIcon from "../svg/CheckIcon";
 import { useRouter } from "next/navigation";
+import LoadingCircle from "../icons/LoadingCircle";
 
 type FormType = NewClientsRequest;
 
@@ -35,6 +36,11 @@ const initialValues: FormType = {
   filenumber: 0,
   phone_number: "",
   profile_picture: "",
+  city: "",
+  Zipcode: "",
+  infix: "",
+  streetname: "",
+  street_number: "",
 };
 
 export const clientsSchema: Yup.ObjectSchema<FormType> = Yup.object().shape({
@@ -50,12 +56,19 @@ export const clientsSchema: Yup.ObjectSchema<FormType> = Yup.object().shape({
   organsation: Yup.string(),
   organisation: Yup.string(),
   gender: Yup.string(),
+  city: Yup.string(),
+  Zipcode: Yup.string(),
+  infix: Yup.string(),
+  streetname: Yup.string(),
+  street_number: Yup.string(),
 });
 
 type PropsType = {};
 
 export const ClientsForm: FunctionComponent<PropsType> = ({}) => {
   const [isSuccess, setIsSuccess] = useState(false);
+
+  const [uploading, setUploading] = useState(false);
 
   const router = useRouter();
 
@@ -66,8 +79,14 @@ export const ClientsForm: FunctionComponent<PropsType> = ({}) => {
 
   const { mutate, isLoading } = useCreateClients(onSuccessCallback);
 
+  const [imagePreviewUrl, setImagePreviewUrl] = useState(
+    "/images/user/user-06.png"
+  );
+
   const onSubmit = useCallback(
     (values: FormType, { resetForm }: FormikHelpers<FormType>) => {
+      setUploading(true);
+
       mutate(values, {
         onSuccess: () => {
           resetForm();
@@ -114,180 +133,280 @@ export const ClientsForm: FunctionComponent<PropsType> = ({}) => {
           values,
           handleChange,
           handleBlur,
+          setFieldValue,
           touched,
           handleSubmit,
           errors,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <div className="p-6.5">
-              <div className="px-4 mt-[70px] text-center">
-                <div className="relative z-30 w-full p-1 mx-auto rounded-full -mt-22 h-30 max-w-30 bg-white/20 backdrop-blur sm:h-44 sm:max-w-44 sm:p-3">
-                  <div className="relative drop-shadow-2">
-                    <Image
-                      src={"/images/user/user-06.png"}
-                      width={160}
-                      height={160}
-                      alt="profile"
-                    />
-                    <label
-                      htmlFor="profile"
-                      className="absolute bottom-0 right-0 flex h-8.5 w-8.5 cursor-pointer items-center justify-center rounded-full bg-primary text-white hover:bg-opacity-90 sm:bottom-2 sm:right-2"
-                    >
-                      <CameraIcon />
-                      <input
-                        type="file"
-                        name="profile"
-                        id="profile"
-                        className="sr-only"
-                      />
-                    </label>
+        }) => {
+          const handleFileChange = async (
+            event: React.ChangeEvent<HTMLInputElement>
+          ) => {
+            const file = event.target.files ? event.target.files[0] : null;
+            if (file) {
+              setUploading(true);
+
+              try {
+                // Generate a preview URL for the selected file
+                const fileUrl = URL.createObjectURL(file);
+                setImagePreviewUrl(fileUrl); // Update the preview URL state
+
+                // Update Formik's state if needed
+                setFieldValue("profile_picture", file);
+
+                setUploading(false);
+              } catch (error) {
+                console.error("Error uploading file:", error);
+                setUploading(false);
+              }
+            }
+          };
+
+          return (
+            <form onSubmit={handleSubmit}>
+              <div className="p-6.5">
+                <div className="px-4 mt-[70px] text-center">
+                  <div className="relative z-30 w-full p-1 mx-auto rounded-full -mt-22 h-30 max-w-30 bg-white/20 backdrop-blur sm:h-44 sm:max-w-44 sm:p-3">
+                    <div className="relative drop-shadow-2">
+                      {uploading ? (
+                        <div className="absolute inset-0 flex items-center justify-center">
+                          <span className="animate-spin">
+                            <LoadingCircle />
+                          </span>
+                        </div>
+                      ) : (
+                        <Image
+                          alt="profile"
+                          width={160}
+                          height={160}
+                          src={imagePreviewUrl}
+                        />
+                      )}
+                      <label
+                        htmlFor="profile"
+                        className="absolute bottom-0 right-0 flex h-8.5 w-8.5 cursor-pointer items-center justify-center rounded-full bg-primary text-white hover:bg-opacity-90 sm:bottom-2 sm:right-2"
+                      >
+                        <CameraIcon />
+                        <input
+                          type="file"
+                          name="profile"
+                          id="profile"
+                          accept="image/jpeg,image/png,image/gif"
+                          className="sr-only"
+                          onChange={handleFileChange}
+                          onBlur={handleBlur}
+                          // error={
+                          //   touched.profile_picture && errors.profile_picture
+                          // }
+                        />
+                      </label>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="mb-4.5 py-4 flex flex-col gap-6 xl:flex-row">
+                <div className="mb-4.5 py-4 flex flex-col gap-6 xl:flex-row">
+                  <InputFieldThin
+                    type={"text"}
+                    label={"First name"}
+                    id={"first_name"}
+                    placeholder={"Enter your first name"}
+                    className="w-full xl:w-1/2"
+                    value={values.first_name}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.first_name && errors.first_name}
+                    required={true}
+                  />
+
+                  <InputFieldThin
+                    type={"text"}
+                    label={"Last name"}
+                    id={"last_name"}
+                    placeholder={"Enter your last name"}
+                    className="w-full xl:w-1/2"
+                    value={values.last_name}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.last_name && errors.last_name}
+                    required={true}
+                  />
+                </div>
+
                 <InputFieldThin
                   type={"text"}
-                  label={"First name"}
-                  id={"firt_name"}
-                  placeholder={"Enter your first name"}
-                  className="w-full xl:w-1/2"
-                  value={values.first_name}
+                  label={"Email"}
+                  id={"email"}
+                  placeholder={"Enter your email address"}
+                  className="w-full mb-4.5"
+                  value={values.email}
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  error={touched.first_name && errors.first_name}
+                  error={touched.email && errors.email}
                   required={true}
                 />
 
                 <InputFieldThin
+                  label={"Phone Number"}
+                  id={"phone_number"}
+                  placeholder={"Phone Number"}
                   type={"text"}
-                  label={"Last name"}
-                  id={"last_name"}
-                  placeholder={"Enter your last name"}
-                  className="w-full xl:w-1/2"
-                  value={values.last_name}
+                  className="w-full mb-4.5"
+                  value={values.phone_number}
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  error={touched.last_name && errors.last_name}
+                  error={touched.phone_number && errors.phone_number}
                   required={true}
                 />
-              </div>
 
-              <InputFieldThin
-                type={"text"}
-                label={"Email"}
-                id={"email"}
-                placeholder={"Enter your email address"}
-                className="w-full mb-4.5"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.email && errors.email}
-                required={true}
-              />
-
-              <InputFieldThin
-                label={"Phone Number"}
-                id={"phone_number"}
-                placeholder={"Phone Number"}
-                type={"text"}
-                className="w-full mb-4.5"
-                value={values.phone_number}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.phone_number && errors.phone_number}
-                required={true}
-              />
-
-              <InputFieldThin
-                label={"Organisation"}
-                id={"organisation"}
-                placeholder={"Organisation"}
-                type={"text"}
-                className="w-full mb-4.5"
-                value={values.organisation}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.organisation && errors.organisation}
-              />
-
-              <InputFieldThin
-                label={"Location"}
-                id={"location"}
-                placeholder={"Location"}
-                type={"text"}
-                className="w-full mb-4.5"
-                value={values.location}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.location && errors.location}
-              />
-
-              <InputFieldThin
-                label={"Departement"}
-                id={"departement"}
-                placeholder={"Departement"}
-                type={"text"}
-                className="w-full mb-4.5"
-                value={values.departement}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.departement && errors.departement}
-              />
-
-              <div className="mb-4.5 bg-white border rounded-sm border-stroke shadow-default dark:border-strokedark dark:bg-boxdark">
-                <div className="border-b border-stroke py-6  px-6.5 dark:border-strokedark">
-                  <h3 className="font-medium text-black dark:text-white">
-                    Gender
-                  </h3>
-                </div>
-                <div className="flex flex-row gap-5.5 p-6.5">
-                  <RadioCustom
-                    id={1}
-                    type="radio"
-                    label="Male"
-                    name="gender"
-                    value="Male"
-                  />
-                  <RadioCustom
-                    id={2}
-                    type="radio"
-                    label={"Female"}
-                    name="gender"
-                    value="Female"
-                  />
-                </div>
-              </div>
-
-              <div className="mb-4.5">
-                <label className="mb-2.5 block text-black dark:text-white">
-                  File Number
-                </label>
-
-                <input
-                  className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none  transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                  // label={"filenumber"}
-                  id={"filenumber"}
-                  placeholder={"File Number"}
-                  type={"number"}
-                  value={values.filenumber}
+                <InputFieldThin
+                  label={"Organisation"}
+                  id={"organisation"}
+                  placeholder={"Organisation"}
+                  type={"text"}
+                  className="w-full mb-4.5"
+                  value={values.organisation}
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  // error={touched.filenumber && errors.filenumber}
+                  error={touched.organisation && errors.organisation}
                 />
-              </div>
 
-              <Button
-                type={"submit"}
-                disabled={isLoading}
-                isLoading={isLoading}
-                formNoValidate={true}
-              >
-                Submit Clients
-              </Button>
-            </div>
-          </form>
-        )}
+                <InputFieldThin
+                  label={"City"}
+                  id={"city"}
+                  placeholder={"City"}
+                  type={"text"}
+                  className="w-full mb-4.5"
+                  value={values.city}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.city && errors.city}
+                />
+
+                <InputFieldThin
+                  label={"Zip code"}
+                  id={"Zipcode"}
+                  placeholder={"Zip code"}
+                  type={"text"}
+                  className="w-full mb-4.5"
+                  value={values.Zipcode}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.Zipcode && errors.Zipcode}
+                />
+
+                <InputFieldThin
+                  label={"Infix"}
+                  id={"infix"}
+                  placeholder={"Infix"}
+                  type={"text"}
+                  className="w-full mb-4.5"
+                  value={values.infix}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.infix && errors.infix}
+                />
+
+                <InputFieldThin
+                  label={"Street Name"}
+                  id={"streetname"}
+                  placeholder={"Street Name"}
+                  type={"text"}
+                  className="w-full mb-4.5"
+                  value={values.streetname}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.streetname && errors.streetname}
+                />
+
+                <InputFieldThin
+                  label={"street Number"}
+                  id={"street_number"}
+                  placeholder={"street Number"}
+                  type={"text"}
+                  className="w-full mb-4.5"
+                  value={values.street_number}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.street_number && errors.street_number}
+                />
+
+                <InputFieldThin
+                  label={"Location"}
+                  id={"location"}
+                  placeholder={"Location"}
+                  type={"text"}
+                  className="w-full mb-4.5"
+                  value={values.location}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.location && errors.location}
+                />
+
+                <InputFieldThin
+                  label={"Departement"}
+                  id={"departement"}
+                  placeholder={"Departement"}
+                  type={"text"}
+                  className="w-full mb-4.5"
+                  value={values.departement}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.departement && errors.departement}
+                />
+
+                <div className="mb-4.5 bg-white border rounded-sm border-stroke shadow-default dark:border-strokedark dark:bg-boxdark">
+                  <div className="border-b border-stroke py-6  px-6.5 dark:border-strokedark">
+                    <h3 className="font-medium text-black dark:text-white">
+                      Gender
+                    </h3>
+                  </div>
+                  <div className="flex flex-row gap-5.5 p-6.5">
+                    <RadioCustom
+                      id={1}
+                      type="radio"
+                      label="Male"
+                      name="gender"
+                      value="Male"
+                    />
+                    <RadioCustom
+                      id={2}
+                      type="radio"
+                      label={"Female"}
+                      name="gender"
+                      value="Female"
+                    />
+                  </div>
+                </div>
+
+                <div className="mb-4.5">
+                  <label className="mb-2.5 block text-black dark:text-white">
+                    File Number
+                  </label>
+
+                  <input
+                    className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none  transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                    // label={"filenumber"}
+                    id={"filenumber"}
+                    placeholder={"File Number"}
+                    type={"number"}
+                    value={values.filenumber}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    // error={touched.filenumber && errors.filenumber}
+                  />
+                </div>
+
+                <Button
+                  type={"submit"}
+                  disabled={isLoading}
+                  isLoading={isLoading}
+                  formNoValidate={true}
+                >
+                  Submit Clients
+                </Button>
+              </div>
+            </form>
+          );
+        }}
       </Formik>
     </>
   );
