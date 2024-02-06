c791fcde1118c7fbca0e227a387f7b2b7e53c294
saadazghour <saad.azghour@gmail.com>
2024-02-06 12:41:55 +0100

    feat(reports-summary): Add UI for displaying latest reports
    - Integrate Loader component to display a loading state while fetching reports, improving user feedback during data retrieval.

--- components/clientDetails/ReportsSummary.tsx
+++ components/clientDetails/ReportsSummary.tsx
@@ -1,11 +1,54 @@
+"use client";
+
+import { ReportsListItem } from "@/types/reports/reports-list-res-dto";
+import { useLatestReports } from "@/utils/ReportsSummary/getLatestReports";
+import { useRouter } from "next/navigation";
+
 import React, { FunctionComponent } from "react";
+import Loader from "@/components/common/Loader";
 
 type Props = {
   clientId: number;
 };
 
 const ReportsSummary: FunctionComponent<Props> = ({ clientId }) => {
-  return <div>ReportsSummary FOR CLIENT OF ID {clientId}</div>;
+  const { data, isLoading } = useLatestReports(clientId, 5);
+
+  if (isLoading) return <Loader />;
+
+  if (data?.length === 0) return <div>No reports yet</div>;
+
+  return (
+    <ul className="flex flex-col gap-2">
+      {data?.map((report) => (
+        <>
+          <ReportsItem key={report.id} reports={report} />
+        </>
+      ))}
+    </ul>
+  );
 };
 
 export default ReportsSummary;
+
+type ReportsItemProps = {
+  reports: ReportsListItem;
+};
+
+const ReportsItem: FunctionComponent<ReportsItemProps> = ({ reports }) => {
+  const router = useRouter();
+
+  return (
+    <li
+      className="grid grid-cols-3 px-4 py-2 cursor-pointer hover:bg-gray-3 rounded-2xl"
+      // onClick={() =>
+      //   router.push(`/clients/${reports.client}/reports/${reports.id}`)
+      // }
+    >
+      <div className="font-medium text-gray-600">{reports.date}</div>
+      <div className="col-span-2 text-gray-600 truncate">
+        {reports.report_text}
+      </div>
+    </li>
+  );
+};
