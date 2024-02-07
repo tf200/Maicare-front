f1d0fac37ee3073b2aedf9c222a42fe2c924550f
saadazghour <saad.azghour@gmail.com>
2024-02-07 10:46:39 +0100

    feat(new-reports-form): Add form component for creating new reports

---
+++ app/(pages)/clients/[clientId]/reports/new/page.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from "react";
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import ReportsForm from "@/components/forms/ReportsForm";
+
+const NewReports: FunctionComponent = ({
+  params,
+}: {
+  params: { clientId: string };
+}) => {
+  return (
+    <>
+      <Breadcrumb pageName="New Reports" />
+
+      <div className="grid grid-cols-1 gap-9 sm:grid-cols-2">
+        <div className="flex flex-col gap-9">
+          {/* <!-- Reports Form --> */}
+          <div className="bg-white border rounded-sm border-stroke shadow-default dark:border-strokedark dark:bg-boxdark">
+            <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
+              <h3 className="font-medium text-black dark:text-white">
+                Create a New Reports
+              </h3>
+            </div>
+            <ReportsForm clientId={params.clientId} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NewReports;
