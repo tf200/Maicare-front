55f95230c5a2764459b5e5aa0a2e43bd450d9cbd
Nouamane Elmkadem <nouamanlamkadmxd@gmail.com>
2024-02-07 12:47:40 +0100

    configured images in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // distDir: "build",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "healtystorages.s3.amazonaws.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
