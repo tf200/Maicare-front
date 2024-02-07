53e4761fd10e198dfc05f7294c232bf00575853f
saadazghour <saad.azghour@gmail.com>
2024-02-07 10:44:24 +0100

    feat(reports-details-page): Initialize for reports details

---
+++ app/(pages)/clients/[clientId]/reports/[reportsId]/page.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from "react";
+
+const ReportsDetailsPage: FunctionComponent = (props) => {
+  return <div>[Reports details page]</div>;
+};
+
+export default ReportsDetailsPage;
