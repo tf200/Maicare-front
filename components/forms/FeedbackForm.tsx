5e59ab58dbba7eb564d2e5dd0b3c0a4b3834fa29
saadazghour <saad.azghour@gmail.com>
2024-02-08 16:24:04 +0100

    feat: Add feedback submission form && validation

---
+++ components/forms/FeedbackForm.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import * as Yup from "yup";
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
+import { Formik } from "formik";
+import { FormikHelpers } from "formik/dist/types";
+import Button from "@/components/buttons/Button";
+import { useRouter } from "next/navigation";
+import { NewFeedbackReqDto } from "@/types/feedback/new-feedback-req-dto";
+import { useCreateFeedback } from "@/utils/feedback/createFeedback";
+import Textarea from "../FormFields/Textarea";
+
+type FormType = NewFeedbackReqDto;
+
+export type FeedbackFormType = FormType;
+
+const initialValues: FormType = {
+  feedback_text: "",
+};
+
+export const feedbackSchema: Yup.ObjectSchema<FormType> = Yup.object().shape({
+  id: Yup.number(),
+  date: Yup.string(),
+  client: Yup.number(),
+  feedback_text: Yup.string().required("Please provide your feedback"),
+});
+
+type PropsType = {
+  clientId: string;
+};
+
+export const FeedbackForm: FunctionComponent<PropsType> = ({ clientId }) => {
+  const { mutate, isLoading } = useCreateFeedback(parseInt(clientId));
+  const [isSuccess, setIsSuccess] = useState(false);
+  const router = useRouter();
+
+  const onSubmit = useCallback(
+    (values: FormType, { resetForm }: FormikHelpers<FormType>) => {
+      mutate(values, {
+        onSuccess: () => {
+          setIsSuccess(true);
+          resetForm();
+        },
+      });
+    },
+    [mutate]
+  );
+
+  useEffect(() => {
+    if (isSuccess) {
+      setTimeout(() => {
+        router.push(`/clients/${clientId}/reports-record/feedback`);
+      }, 3000);
+    }
+  }, [isSuccess, router]);
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={feedbackSchema}
+    >
+      {({
+        values,
+        handleChange,
+        handleBlur,
+        touched,
+        handleSubmit,
+        errors,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <div className="p-6.5">
+            <Textarea
+              className={"w-full mb-4.5"}
+              id={"feedback"}
+              name={"feedback_text"}
+              rows={6}
+              label={"Feedback"}
+              placeholder={"Provide feedback please!"}
+              value={values.feedback_text}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={touched.feedback_text && errors.feedback_text}
+              required={true}
+            />
+
+            <Button
+              type={"submit"}
+              disabled={isLoading}
+              isLoading={isLoading}
+              formNoValidate={true}
+              loadingText={"Submitting Feedback..."}
+            >
+              Submit Feedback
+            </Button>
+          </div>
+        </form>
+      )}
+    </Formik>
+  );
+};
+
+export default FeedbackForm;
