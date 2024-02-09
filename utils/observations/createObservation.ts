45ecb93e3482f7885f1f12fbbc26949582cc20e5
saadazghour <saad.azghour@gmail.com>
2024-02-09 16:00:44 +0100

    feat: Add createObservation functionality && react-query integration

---
+++ utils/observations/createObservation.ts
@@ -0,0 +1,33 @@
+import api from "@/utils/api";
+import { useMutation, useQueryClient } from "react-query";
+import { NewObservationsReqDto } from "@/types/observations/new-observations-req-dto";
+import { ObservationsResDto } from "../../types/observations/observations-res-dto";
+import { ObservationFormType } from "@/components/forms/ObservationForm";
+
+async function createObservation(
+  data: NewObservationsReqDto
+): Promise<ObservationsResDto> {
+  const response = await api.post<ObservationsResDto>(
+    "employee/observations_cl/",
+    data
+  );
+
+  return response.data;
+}
+
+export const useCreateObservation = (client: number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: ObservationFormType) => {
+      return createObservation({
+        ...data,
+        client,
+      });
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries([client, "observation"]);
+    },
+  });
+};
