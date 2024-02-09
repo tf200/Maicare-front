9ff9ac8489a65d825995009c51a73fbb3e018d3f
mellouki-atlasai <156778978+mellouki-atlasai@users.noreply.github.com>
2024-02-09 17:52:02 +0100

    modify y padding for alert message

--- components/LargeErrorMessage.tsx
+++ components/LargeErrorMessage.tsx
@@ -10,7 +10,7 @@ const LargeErrorMessage: FunctionComponent<Props> = ({
   secondLine,
 }) => {
   return (
-    <div className="rounded-sm border border-stroke bg-white px-5 py-10 dark:border-strokedark dark:bg-boxdark sm:py-20">
+    <div className="rounded-sm border border-stroke bg-white px-5 py-5 dark:border-strokedark dark:bg-boxdark sm:py-5">
       <div className="mx-auto max-w-[410px]">
         <Image
           src={"/images/illustration/illustration-01.svg"}
