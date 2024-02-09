ff4918209fc3c31dace13e1526b7635fc95d14a9
saadazghour <saad.azghour@gmail.com>
2024-02-09 16:00:02 +0100

    feat: Implement NewObservation page with form && adding observations

---
+++ app/(pages)/clients/[clientId]/observations/new/page.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from "react";
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import ObservationForm from "@/components/forms/ObservationForm";
+
+const NewObservation: FunctionComponent = ({
+  params,
+}: {
+  params: { clientId: string };
+}) => {
+  return (
+    <>
+      <Breadcrumb pageName="New Observation" />
+
+      <div className="grid grid-cols-1 gap-9 sm:grid-cols-2">
+        <div className="flex flex-col gap-9">
+          {/* <!-- Observation Form --> */}
+          <div className="bg-white border rounded-sm border-stroke shadow-default dark:border-strokedark dark:bg-boxdark">
+            <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
+              <h3 className="font-medium text-black dark:text-white">
+                Add New Observation
+              </h3>
+            </div>
+            <ObservationForm clientId={params.clientId} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NewObservation;
