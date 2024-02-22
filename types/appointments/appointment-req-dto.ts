8365db2e0c155f5cf2c984100c6099c886c3b72d
mellouki-atlasai <156778978+mellouki-atlasai@users.noreply.github.com>
2024-02-22 17:18:17 +0100

    add appointment invitees

--- types/appointments/appointment-req-dto.ts
+++ types/appointments/appointment-req-dto.ts
@@ -1,3 +1,9 @@
 import { AppointmentFormType } from "@/types/appointments/appointment-form-type";
 
-export type NewAppointmentReqDto = AppointmentFormType;
+export type NewAppointmentReqDto = Omit<
+  AppointmentFormType,
+  "employees" | "clients"
+> & {
+  employees: number[];
+  clients: number[];
+};
