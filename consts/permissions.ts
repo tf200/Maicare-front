dbb08cbcad4a89cee4827cde8bfe8dbb62c35eef
Nouamane Elmkadem <nouamanlamkadmxd@gmail.com>
2024-03-14 15:19:48 +0000

    PERMISSIONS UPDATE

--- consts/permissions.ts
+++ consts/permissions.ts
@@ -1,34 +1,33 @@
-// export const CLIENT_VIEW = "client.view";
-// export const CLIENT_EDIT = "client.edit";
-// export const CLIENT_DELETE = "client.delete";
-// export const CLIENT_CREATE = "client.create";
-
-// export const CLIENT_CONTRACT_CRUD = "client_contract.view";
-// export const CLIENT_CONTRACT_EDIT = "client_contract.edit";
-// export const CLIENT_CONTRACT_DELETE = "client_contract.delete";
-// export const CLIENT_CONTRACT_CREATE = "client_contract.create";
-
-export const CLIENT_CRUD = "client";
 export const CLIENT_VIEW = "client.view";
+export const CLIENT_EDIT = "client.edit";
+export const CLIENT_DELETE = "client.delete";
+export const CLIENT_CREATE = "client.create";
 
-export const EMPLOYEE_CRUD = "employee";
 export const EMPLOYEE_VIEW = "employee.view";
+export const EMPLOYEE_EDIT = "employee.edit";
+export const EMPLOYEE_DELETE = "employee.delete";
+export const EMPLOYEE_CREATE = "employee.create";
 
-export const CLIENT_CONTRACT_CRUD = "client_contract";
-export const EMPLOYEE_CONTRACT_CRUD = "employee_contract";
-export const CARE_COORDINATION_CRUD = "care_coordination";
-export const FINANCE_CRUD = "finance";
-
+// export const CLIENT_CONTRACT_CRUD = "client_contract";
+// export const EMPLOYEE_CONTRACT_CRUD = "employee_contract";
+// export const CARE_COORDINATION_CRUD = "care_coordination";
+// export const FINANCE_CRUD = "finance";
 
 export const PERMISSIONS = [
-  CLIENT_CRUD,
   CLIENT_VIEW,
-  EMPLOYEE_CRUD,
+  CLIENT_EDIT,
+  CLIENT_DELETE,
+  CLIENT_CREATE,
+
   EMPLOYEE_VIEW,
-  CLIENT_CONTRACT_CRUD,
-  EMPLOYEE_CONTRACT_CRUD,
-  CARE_COORDINATION_CRUD,
-  FINANCE_CRUD
+  EMPLOYEE_EDIT,
+  EMPLOYEE_DELETE,
+  EMPLOYEE_CREATE,
+
+  // CLIENT_CONTRACT_CRUD,
+  // EMPLOYEE_CONTRACT_CRUD,
+  // CARE_COORDINATION_CRUD,
+  // FINANCE_CRUD,
 ] as const;
 
 export const ADMIN = "ADMIN";
@@ -54,10 +53,10 @@ export const PERMISSION_CONFIGURATIONS: Record<
   (typeof PERMISSIONS)[number][]
 > = {
   ADMIN: [...PERMISSIONS],
-  BEHAVIORAL_SPECIALIST: [CLIENT_CRUD, EMPLOYEE_CRUD],
-  MANAGEMENT: [CLIENT_VIEW, EMPLOYEE_VIEW],
+  BEHAVIORAL_SPECIALIST: [CLIENT_VIEW, CLIENT_EDIT, EMPLOYEE_VIEW, EMPLOYEE_EDIT],
+  MANAGEMENT: [CLIENT_VIEW, CLIENT_EDIT, EMPLOYEE_VIEW, EMPLOYEE_EDIT],
   TRAJECTORY_GUIDE: [CLIENT_VIEW],
   PEDAGOGICAL_WORKER: [CLIENT_VIEW],
-  BACK_OFFICE: [CLIENT_CRUD, EMPLOYEE_CRUD],
-  ADMINISTRATIVE_ASSISTANT: [CLIENT_CRUD, EMPLOYEE_CRUD],
+  BACK_OFFICE: [CLIENT_VIEW, CLIENT_EDIT, CLIENT_CREATE, EMPLOYEE_VIEW, EMPLOYEE_EDIT, EMPLOYEE_CREATE],
+  ADMINISTRATIVE_ASSISTANT: [CLIENT_VIEW, CLIENT_EDIT, CLIENT_CREATE, EMPLOYEE_VIEW, EMPLOYEE_EDIT, EMPLOYEE_CREATE],
 };
