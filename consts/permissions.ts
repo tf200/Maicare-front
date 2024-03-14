a023980c125680ff5772bd247f013f50643bde9f
Nouamane Elmkadem <nouamanlamkadmxd@gmail.com>
2024-03-14 14:52:41 +0000

    permissions configuration

--- consts/permissions.ts
+++ consts/permissions.ts
@@ -1,21 +1,34 @@
+// export const CLIENT_VIEW = "client.view";
+// export const CLIENT_EDIT = "client.edit";
+// export const CLIENT_DELETE = "client.delete";
+// export const CLIENT_CREATE = "client.create";
+
+// export const CLIENT_CONTRACT_CRUD = "client_contract.view";
+// export const CLIENT_CONTRACT_EDIT = "client_contract.edit";
+// export const CLIENT_CONTRACT_DELETE = "client_contract.delete";
+// export const CLIENT_CONTRACT_CREATE = "client_contract.create";
+
+export const CLIENT_CRUD = "client";
 export const CLIENT_VIEW = "client.view";
-export const CLIENT_EDIT = "client.edit";
-export const CLIENT_DELETE = "client.delete";
-export const CLIENT_CREATE = "client.create";
-export const CLIENT_CONTRACT_VIEW = "client_contract.view";
-export const CLIENT_CONTRACT_EDIT = "client_contract.edit";
-export const CLIENT_CONTRACT_DELETE = "client_contract.delete";
-export const CLIENT_CONTRACT_CREATE = "client_contract.create";
+
+export const EMPLOYEE_CRUD = "employee";
+export const EMPLOYEE_VIEW = "employee.view";
+
+export const CLIENT_CONTRACT_CRUD = "client_contract";
+export const EMPLOYEE_CONTRACT_CRUD = "employee_contract";
+export const CARE_COORDINATION_CRUD = "care_coordination";
+export const FINANCE_CRUD = "finance";
+
 
 export const PERMISSIONS = [
+  CLIENT_CRUD,
   CLIENT_VIEW,
-  CLIENT_EDIT,
-  CLIENT_DELETE,
-  CLIENT_CREATE,
-  CLIENT_CONTRACT_VIEW,
-  CLIENT_CONTRACT_EDIT,
-  CLIENT_CONTRACT_DELETE,
-  CLIENT_CONTRACT_CREATE,
+  EMPLOYEE_CRUD,
+  EMPLOYEE_VIEW,
+  CLIENT_CONTRACT_CRUD,
+  EMPLOYEE_CONTRACT_CRUD,
+  CARE_COORDINATION_CRUD,
+  FINANCE_CRUD
 ] as const;
 
 export const ADMIN = "ADMIN";
@@ -41,10 +54,10 @@ export const PERMISSION_CONFIGURATIONS: Record<
   (typeof PERMISSIONS)[number][]
 > = {
   ADMIN: [...PERMISSIONS],
-  BEHAVIORAL_SPECIALIST: [CLIENT_VIEW, CLIENT_EDIT, CLIENT_CREATE],
-  MANAGEMENT: [CLIENT_VIEW, CLIENT_EDIT, CLIENT_CREATE],
-  TRAJECTORY_GUIDE: [],
-  PEDAGOGICAL_WORKER: [],
-  BACK_OFFICE: [],
-  ADMINISTRATIVE_ASSISTANT: [],
+  BEHAVIORAL_SPECIALIST: [CLIENT_CRUD, EMPLOYEE_CRUD],
+  MANAGEMENT: [CLIENT_VIEW, EMPLOYEE_VIEW],
+  TRAJECTORY_GUIDE: [CLIENT_VIEW],
+  PEDAGOGICAL_WORKER: [CLIENT_VIEW],
+  BACK_OFFICE: [CLIENT_CRUD, EMPLOYEE_CRUD],
+  ADMINISTRATIVE_ASSISTANT: [CLIENT_CRUD, EMPLOYEE_CRUD],
 };
