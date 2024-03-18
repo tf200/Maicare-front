99d42abb5d2c1109050dcc12947c3d67c97524c3
Nouamane Elmkadem <nouamanlamkadmxd@gmail.com>
2024-03-18 12:49:00 +0000

    Profile Updated

--- app/(pages)/employees/[employeeId]/page.tsx
+++ app/(pages)/employees/[employeeId]/page.tsx
@@ -1,24 +1,7 @@
 "use client";
 import React, { FunctionComponent, useEffect } from "react";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import Panel from "@/components/Panel";
-import EmployeeInformation from "@/components/EmployeeDetails/EmployeeInformation";
-import EmployeeCertificationsSummary from "@/components/EmployeeDetails/EmployeeCertificationsSummary";
-import EmployeeEducationsSummary from "@/components/EmployeeDetails/EmployeeEducationsSummary";
-import EmployeeExperiencesSummary from "@/components/EmployeeDetails/EmployeeExperiencesSummary";
-import EmployeeRolesSummary from "@/components/EmployeeDetails/EmployeeRolesSummary";
-import LinkButton from "@/components/buttons/LinkButton";
-import Link from "next/link";
-import IconButton from "@/components/buttons/IconButton";
-import PencilSquare from "@/components/icons/PencilSquare";
-import TrashIcon from "@/components/icons/TrashIcon";
-import CheckIcon from "@/components/icons/CheckIcon";
-import { useModal } from "@/components/providers/ModalProvider";
-import { getDangerActionConfirmationModal } from "@/components/Modals/DangerActionConfirmation";
-import { useDeleteEmployee } from "@/utils/employees/deleteEmployee";
-import { useRouter } from "next/navigation";
-import { SecureFragment } from "@/components/SecureWrapper";
-import * as consts from "@/consts/permissions";
+import EmployeeDetails from "@/components/EmployeeDetails";
 
 type Props = {
   params: {
@@ -29,124 +12,10 @@ type Props = {
 const EmployeeDetailsPage: FunctionComponent<Props> = ({
   params: { employeeId },
 }) => {
-  const router = useRouter();
-
-  const {
-    mutate: deleteEmployee,
-    isLoading: isDeleting,
-    isSuccess: isDeleted,
-  } = useDeleteEmployee();
-
-  useEffect(() => {
-    if (isDeleted) {
-      setTimeout(() => {
-        router.push(`/employees`);
-      }, 700);
-    }
-  }, [isDeleted]);
-
-  const { open } = useModal(
-    getDangerActionConfirmationModal({
-      msg: "Weet u zeker dat u deze medewerker wilt verwijderen?",
-      title: "Medewerker Verwijderen",
-    })
-  );
-
   return (
     <>
       <Breadcrumb pageName="Medewerkerdetails" />
-      <div className="grid grid-cols-1 gap-9 sm:grid-cols-2">
-        <div className="flex flex-col gap-9">
-          <Panel
-            title={"Medewerkerinformatie"}
-            containerClassName="px-7 py-4"
-            sideActions={
-              <div className="flex gap-4">
-                <SecureFragment permission={consts.EMPLOYEE_EDIT}>
-                  <Link href={`/employees/${employeeId}/edit`}>
-                    <IconButton>
-                      <PencilSquare className="w-5 h-5" />
-                    </IconButton>
-                  </Link>
-                </SecureFragment>
-
-                <SecureFragment permission={consts.EMPLOYEE_DELETE}>
-                  <IconButton
-                    buttonType="Danger"
-                    onClick={() => {
-                      open({
-                        onConfirm: () => {
-                          deleteEmployee(parseInt(employeeId));
-                        },
-                      });
-                    }}
-                    disabled={isDeleted}
-                    isLoading={isDeleting}
-                  >
-                    {isDeleted ? (
-                      <CheckIcon className="w-5 h-5" />
-                    ) : (
-                      <TrashIcon className="w-5 h-5" />
-                    )}
-                  </IconButton>
-                </SecureFragment>
-              </div>
-            }
-          >
-            <EmployeeInformation employeeId={parseInt(employeeId)} />
-          </Panel>
-          <Panel
-            title={"Certificaten"}
-            containerClassName="px-7 py-4"
-            sideActions={
-              <LinkButton
-                text={"Volledige Certificatenlijst"}
-                href={`/employees/${employeeId}/certificates`}
-              />
-            }
-          >
-            <EmployeeCertificationsSummary employeeId={parseInt(employeeId)} />
-          </Panel>
-        </div>
-        <div className="flex flex-col gap-9">
-          <Panel
-            title={"Opleidingen"}
-            containerClassName="px-7 py-4"
-            sideActions={
-              <LinkButton
-                text={"Volledige Opleidingenlijst"}
-                href={`/employees/${employeeId}/educations`}
-              />
-            }
-          >
-            <EmployeeEducationsSummary employeeId={parseInt(employeeId)} />
-          </Panel>
-          <Panel
-            title={"Ervaringen"}
-            containerClassName="px-7 py-4"
-            sideActions={
-              <LinkButton
-                text={"Volledige Ervaringenlijst"}
-                href={`/employees/${employeeId}/experiences`}
-              />
-            }
-          >
-            <EmployeeExperiencesSummary employeeId={parseInt(employeeId)} />
-          </Panel>
-          <Panel
-            title={"Rollen"}
-            containerClassName="px-7 py-4"
-            sideActions={
-              <LinkButton
-                text={"Volledige Rollijst"}
-                href={`/employees/${employeeId}/teams`}
-              />
-            }
-          >
-            <EmployeeRolesSummary employeeId={parseInt(employeeId)} />
-          </Panel>
-        </div>
-      </div>
+      <EmployeeDetails employeeId={employeeId} />
     </>
   );
 };
