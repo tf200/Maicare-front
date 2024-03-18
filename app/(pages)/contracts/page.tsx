b9aedcff1f8bac26ca9eeb655375472ba89e2f6d
mellouki-atlasai <156778978+mellouki-atlasai@users.noreply.github.com>
2024-03-18 12:23:50 +0000

    add modal to redirect to a client contract creation page

--- app/(pages)/contracts/page.tsx
+++ app/(pages)/contracts/page.tsx
@@ -1,19 +1,32 @@
 "use client";
-import React from "react";
+import React, { useCallback } from "react";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import Panel from "@/components/Panel";
 import LinkButton from "@/components/buttons/LinkButton";
 import { useContractsList } from "@/utils/contracts/getContractsList";
 import ContractsList from "@/components/ContractsList";
+import Button from "@/components/buttons/Button";
+import ClientSelectModal from "@/components/Modals/ClientSelectModal";
+import { useModal } from "@/components/providers/ModalProvider";
+import { useRouter } from "next/navigation";
 
 const Finances = () => {
   const queryResult = useContractsList();
+  const { open } = useModal(ClientSelectModal);
+  const router = useRouter();
+  const selectClient = useCallback(() => {
+    open({
+      onSelect: (clientId) => {
+        router.push(`/clients/${clientId}/contracts/new/`);
+      },
+    });
+  }, [open, router]);
   return (
     <>
       <Breadcrumb pageName="Contracten" />
       <Panel
         title={"Contracten"}
-        sideActions={<LinkButton href={"/clients"} text="Nieuw contract" />}
+        sideActions={<Button onClick={selectClient}>Nieuw contract</Button>}
       >
         <ContractsList queryResult={queryResult} />
       </Panel>
---
+++ components/Modals/ClientSelectModal.tsx
@@ -0,0 +1,65 @@
+import React, { FunctionComponent, useMemo, useState } from "react";
+import { ModalProps } from "@/types/modal-props";
+import FormModal from "@/components/Modals/FormModal";
+import Combobox from "@/components/FormFields/Combobox";
+import { useClientsList } from "@/utils/clients/getClientsList";
+import { ClientsSearchParams } from "@/types/clients/clients-search-params";
+import { FormikProvider, useFormik } from "formik";
+import Button from "@/components/buttons/Button";
+
+const initialValues = {
+  client: "",
+};
+
+const ClientSelectModal: FunctionComponent<ModalProps> = ({
+  open,
+  onClose,
+  additionalProps,
+}) => {
+  const [filter, setFilter] = useState<ClientsSearchParams>({
+    search: "",
+    status__in: "",
+  });
+  const { data } = useClientsList(filter);
+  const options = useMemo(() => {
+    if (!data) {
+      return [];
+    }
+    return data?.results.map((client) => ({
+      label: client.first_name + " " + client.last_name,
+      value: client,
+    }));
+  }, [data]);
+  const formik = useFormik({
+    initialValues,
+    onSubmit: () => {
+      onClose();
+      additionalProps?.onSelect?.(formik.values.client);
+    },
+  });
+  return (
+    <FormModal open={open} onClose={onClose} title={"Selecteer een klant"}>
+      <FormikProvider value={formik}>
+        <form onSubmit={formik.handleSubmit}>
+          <Combobox
+            name="client"
+            className={"mb-5"}
+            placeholder={"Zoek klant..."}
+            options={options}
+            displayValue={(value) => value.first_name + " " + value.last_name}
+            handleQueryChange={(e) => {
+              const search = e.target.value;
+              setFilter({ ...filter, search });
+            }}
+            label={"Klant"}
+          />
+          <Button type={"submit"} formNoValidate={false}>
+            Selecteer klant
+          </Button>
+        </form>
+      </FormikProvider>
+    </FormModal>
+  );
+};
+
+export default ClientSelectModal;
