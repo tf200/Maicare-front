0b008af7bdda4a897854838ce8c915670e32b865
mellouki-atlasai <156778978+mellouki-atlasai@users.noreply.github.com>
2024-04-05 16:25:01 +0000

    fix datetime

--- components/clientDetails/ReportsSummary.tsx
+++ components/clientDetails/ReportsSummary.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import React, { FunctionComponent } from "react";
 import Loader from "@/components/common/Loader";
 import { useLatestReports } from "@/utils/reports/getLatestReports";
+import { shortDateTimeFormat } from "@/utils/timeFormatting";
 
 type Props = {
   clientId: number;
@@ -17,7 +18,9 @@ const ReportsSummary: FunctionComponent<Props> = ({ clientId }) => {
   if (isLoading) return <Loader />;
   if (isError)
     return (
-      <div className="text-red">Een fout heeft ons verhinderd gegevens te laden.</div>
+      <div className="text-red">
+        Een fout heeft ons verhinderd gegevens te laden.
+      </div>
     );
   if (!data) return <div>Geen gegevens opgehaald.</div>;
 
@@ -44,13 +47,10 @@ const ReportsItem: FunctionComponent<ReportsItemProps> = ({ reports }) => {
   const router = useRouter();
 
   return (
-    <li
-      className="grid grid-cols-3 px-4 py-2 cursor-pointer hover:bg-gray-3 rounded-2xl"
-      // onClick={() =>
-      //   router.push(`/clients/${reports.client}/reports/${reports.id}`)
-      // }
-    >
-      <div className="font-medium text-gray-600">{reports.date}</div>
+    <li className="grid grid-cols-3 px-4 py-2 cursor-pointer hover:bg-gray-3 rounded-2xl">
+      <div className="font-medium text-gray-600">
+        {shortDateTimeFormat(reports.date)}
+      </div>
       <div className="col-span-2 text-gray-600 truncate">
         {reports.report_text}
       </div>
