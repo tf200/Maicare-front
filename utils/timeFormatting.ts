cbbe363db2f361ee80cd4494f5ae945b3820206d
mellouki-atlasai <156778978+mellouki-atlasai@users.noreply.github.com>
2024-04-05 16:25:01 +0000

    fix datetime

--- utils/timeFormatting.ts
+++ utils/timeFormatting.ts
@@ -19,6 +19,11 @@ export function fullDateFormat(date: string | Date) {
   return dayjs(date).format(FULL_DATE_FORMAT);
 }
 
+export const SHORT_DATE_TIME_FORMAT = "D MMM YY, HH:mm";
+export function shortDateTimeFormat(date: string | Date) {
+  return dayjs(date).format(SHORT_DATE_TIME_FORMAT);
+}
+
 export const FULL_DATE_TIME_FORMAT = "dddd DD MMMM YYYY HH:mm";
 export function fullDateTimeFormat(date: string | Date) {
   return dayjs(date).format(FULL_DATE_TIME_FORMAT);
