9acae8af90f2224c95727cbf05210bed5692b666
mellouki-atlasai <156778978+mellouki-atlasai@users.noreply.github.com>
2024-04-24 18:27:42 +0100

    heighlight undocumented medication record

--- types/medications/medications-res-dto.ts
+++ types/medications/medications-res-dto.ts
@@ -2,4 +2,5 @@ import { NewMedicationReqDto } from "@/types/medications/new-medication-req-dto"
 
 export type MedicationsResDto = NewMedicationReqDto & {
   id: number;
+  unset_medications: number;
 };
