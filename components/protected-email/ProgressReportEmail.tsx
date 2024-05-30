3f4313b0e43c3867ac2fafd1485054e4b518d82d
Hamza <37643427+essolami@users.noreply.github.com>
2024-05-30 11:11:00 +0100

    styling

--- components/protected-email/ProgressReportEmail.tsx
+++ components/protected-email/ProgressReportEmail.tsx
@@ -10,30 +10,43 @@ const ProgressReportEmail = ({ emailContent: { metadata, subject } }) => {
   if (isLoading) return <Loader />;
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-3xl">
-        <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Progress Report</h2>
-        <div className="space-y-4">
-          <p className="text-lg">
-            <strong>Subject:</strong> {subject}
-          </p>
-          <p className="text-lg">
-            <strong>Full Name:</strong> {progressReportData.full_name}
-          </p>
-          <p className="text-lg">
-            <strong>Report Date:</strong> {new Date(progressReportData.date).toLocaleDateString()}
-          </p>
-          <p className="text-lg">
-            <strong>Type:</strong>{" "}
-            {getLabelFormvalue(DAILY_REPORT_TYPES_OPTIONS, progressReportData.type)}
-          </p>
-          <p className="text-lg">
-            <strong>Emotional State:</strong>{" "}
-            {getLabelFormvalue(EMOTIONAL_STATE_OPTIONS, progressReportData.emotional_state)}
-          </p>
-          <p className="text-lg">
-            <strong>Report Text:</strong> {progressReportData.report_text}
-          </p>
+    <div className="flex items-center justify-center min-h-screen p-6">
+      <div className="bg-white p-10 rounded-lg shadow-xl w-full max-w-3xl">
+        <h2 className="text-4xl font-bold mb-10 text-center text-gray-800">Voortgangsrapport</h2>
+        <div className="space-y-6 text-gray-700">
+          <div className="text-center px-4 py-2 rounded-md border border-gray">
+            <p className="text-xl">
+              <strong>Onderwerp:</strong> {subject}
+            </p>
+          </div>
+          <div className="text-center px-4 py-2 rounded-md border border-gray">
+            <p className="text-xl">
+              <strong>Voor-en achternaam:</strong> {progressReportData.full_name}
+            </p>
+          </div>
+          <div className="text-center px-4 py-2 rounded-md border border-gray">
+            <p className="text-xl">
+              <strong>Rapportdatum:</strong>{" "}
+              {new Date(progressReportData.date).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="text-center px-4 py-2 rounded-md border border-gray">
+            <p className="text-xl">
+              <strong>Type:</strong>{" "}
+              {getLabelFormvalue(DAILY_REPORT_TYPES_OPTIONS, progressReportData.type)}
+            </p>
+          </div>
+          <div className="text-center px-4 py-2 rounded-md border border-gray">
+            <p className="text-xl">
+              <strong>Emotionele staat:</strong>{" "}
+              {getLabelFormvalue(EMOTIONAL_STATE_OPTIONS, progressReportData.emotional_state)}
+            </p>
+          </div>
+          <div className="text-center px-4 py-2 ">
+            <p className="text-xl ">
+              <strong>Rapporteer tekst:</strong> {progressReportData.report_text}
+            </p>
+          </div>
         </div>
       </div>
     </div>
