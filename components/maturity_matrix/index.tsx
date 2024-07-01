ba626af391143357670adfd22dc5920cb1af7252
medram <mramouchy@gmail.com>
2024-07-01 13:16:46 +0100

    Lucide icons added

--- components/maturity_matrix/index.tsx
+++ components/maturity_matrix/index.tsx
@@ -1,5 +1,9 @@
 import { cn } from "@/utils/cn";
-import { useDomains } from "@/utils/domains";
+import { useClientLevels, useDomains } from "@/utils/domains";
+import { DomainLevel, DomainLevels, MDomain } from "@/types/domains";
+import { useCallback, useEffect, useState } from "react";
+import { SetDomainLevelReqDto } from "@/types/goals";
+import Icon from "../Icon";
 
 const GRADIENT_COLORS = [
   "bg-meta-7/[0.4]",
@@ -9,8 +13,15 @@ const GRADIENT_COLORS = [
   "bg-meta-3/[0.4]",
 ];
 
+type MLevel = {
+  level: number;
+  assessments: string;
+};
+
 type ClientMaturityMatrixProps = {
   clientId: number;
+  onDomainLevelsChange?: (domainLevels: SetDomainLevelReqDto[]) => void;
+  onChange?: (selectedDomains: SetDomainLevelReqDto) => void;
 };
 
 const M_LEVELS = [
@@ -21,12 +32,85 @@ const M_LEVELS = [
   "5 Volledig zelfredzaam",
 ] as const;
 
-export default function ClientMaturityMatrix({ clientId }: ClientMaturityMatrixProps) {
+export default function ClientMaturityMatrix({
+  clientId,
+  onDomainLevelsChange,
+  onChange,
+}: ClientMaturityMatrixProps) {
   const { data: domains, isLoading } = useDomains();
+  const { data: clientLevels, isLoading: isLoadingClientLevels } = useClientLevels(clientId);
 
-  if (isLoading) {
+  const [selectedDomains, setSelectedDomains] = useState<SetDomainLevelReqDto[]>(
+    clientLevels
+      ? clientLevels.map((domainLevel) => ({
+          domain_id: domainLevel.domain_id,
+          level: domainLevel.level,
+        }))
+      : []
+  );
+
+  useEffect(() => {
+    if (clientLevels) {
+      setSelectedDomains(
+        // parse domain levels to SetDomainLevelReqDto
+        clientLevels.map((domainLevel) => ({
+          domain_id: domainLevel.domain_id,
+          level: domainLevel.level,
+        }))
+      );
+    }
+  }, [clientLevels]);
+
+  useEffect(() => {
+    if (selectedDomains && onDomainLevelsChange) {
+      onDomainLevelsChange(selectedDomains);
+    }
+  }, [selectedDomains]);
+
+  const handleDomainLevelChange = useCallback(
+    (selectedDomainLevel: SetDomainLevelReqDto, domain: MDomain, level: MLevel) => {
+      setSelectedDomains((prev) => {
+        // Not undefined
+        if (prev) {
+          // if (isClientLevelSelected(prev, domain.id, level.level)) {
+          //   return prev.filter(
+          //     (clientLevel) =>
+          //       !(clientLevel.domain_id === domain.id && clientLevel.level === level.level)
+          //   );
+          // }
+
+          // Remove the old level if it exists for the same domain
+          prev = prev.filter((domainLevel) => domainLevel.domain_id !== domain.id);
+
+          onChange?.(selectedDomainLevel);
+
+          return [...prev, selectedDomainLevel];
+        }
+      });
+    },
+    []
+  );
+
+  const handleDomainLevelRemove = useCallback(
+    (selectedDomainLevel: SetDomainLevelReqDto, domain: MDomain, level: MLevel) => {
+      setSelectedDomains((prev) => {
+        if (prev) {
+          return prev.filter(
+            (clientLevel) =>
+              !(clientLevel.domain_id === domain.id && clientLevel.level === level.level)
+          );
+        }
+      });
+    },
+    []
+  );
+
+  if (isLoading || isLoadingClientLevels || selectedDomains === undefined) {
     return <div>Loading...</div>;
   }
+
+  // const DOMAIN_NAMES = domains.map((domain) => domain.name);
+  // const DOMAIN_IDS = domains.map((domain) => domain.id);
 
   return (
     <table className="table-fixed w-full">
@@ -57,11 +141,24 @@ export default function ClientMaturityMatrix({ clientId }: ClientMaturityMatrixP
                 key={level.level}
                 className="align-top w-1/6 border border-stroke whitespace-pre-wrap"
               >
-                <MatrixColumn selected={Math.random() > 0.5}>
+                <MatrixItem
+                  selected={isClientLevelSelected(selectedDomains, domain.id, level.level)}
+                  domainId={domain.id}
+                  levelId={level.level}
+                  onRemove={(selectedDomainLevel) =>
+                    handleDomainLevelRemove(selectedDomainLevel, domain, level)
+                  }
+                  onClick={
+                    isClientLevelSelected(selectedDomains, domain.id, level.level)
+                      ? () => {}
+                      : (selectedDomainLevel) =>
+                          handleDomainLevelChange(selectedDomainLevel, domain, level)
+                  }
+                >
                   {parseAssessments(level.assessments).map((goal, index) => {
                     return <p key={index}>◾ {goal}</p>;
                   })}
-                </MatrixColumn>
+                </MatrixItem>
               </td>
             ))}
           </tr>
@@ -80,21 +177,67 @@ function parseAssessments(assessments: string) {
   }
 }
 
-function MatrixColumn({
+function MatrixItem({
   children,
   selected = false,
+  domainId,
+  levelId,
+  onClick,
+  onRemove,
 }: {
   children: React.ReactNode;
+  domainId: number;
+  levelId: number;
   selected?: boolean;
+  onClick: (selectedDomainLevel: SetDomainLevelReqDto) => void;
+  onRemove?: (selectedDomainLevel: SetDomainLevelReqDto) => void;
 }) {
   return (
     <div
       className={cn(
-        " p-2 min-h-[250px]",
-        selected && "border-4 border-dashed bg-purple-100 border-purple-300 text-black"
+        " p-2 min-h-[250px] cursor-pointer relative",
+        selected &&
+          "border-2 border-dashed bg-purple-100 border-purple-500 text-black cursor-default",
+        !selected && "hover:bg-gray"
       )}
+      onClick={() =>
+        onClick({
+          domain_id: domainId,
+          level: levelId,
+        })
+      }
     >
       {children}
+      {selected && (
+        <span className="absolute right-2 bottom-2">
+          <Icon name="flag" size={23} className="text-purple-500" />
+        </span>
+      )}
+
+      {selected && (
+        <span
+          className="absolute right-2 top-2 rounded-full hover:bg-purple-300 text-purple-500 cursor-pointer"
+          onClick={() =>
+            onRemove({
+              domain_id: domainId,
+              level: levelId,
+            })
+          }
+        >
+          <Icon name="x" size={23} />
+        </span>
+      )}
     </div>
+  );
+}
+
+function isClientLevelSelected(
+  clientLevels: DomainLevels | SetDomainLevelReqDto[],
+  domainId: number,
+  levelId: number
+) {
+  return clientLevels.some(
+    (clientLevel: DomainLevel) =>
+      clientLevel.domain_id === domainId && clientLevel.level === levelId
   );
 }
