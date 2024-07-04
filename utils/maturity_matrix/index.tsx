c4214fbe8e009eea576d148e9a88e12f09248dc3
medram <mramouchy@gmail.com>
2024-07-04 17:56:45 +0100

    new util added: uppercase the first letter

--- utils/maturity_matrix/index.tsx
+++ utils/maturity_matrix/index.tsx
@@ -63,13 +63,77 @@ export type SmartFormulaResponse = {
   goals: SmartFormulaGoal[];
 };
 
-async function getSmartFormula(clientId: number, domainId: number, levelId: number) {
+export type EditedSmartFormulaResponse = {
+  goals: EditedSmartFormulaGoal[];
+};
+
+export type EditedSmartFormulaObjective = {
+  title: string; // this is the objective title
+  description: string; // this is the objective description (which includes all objectives' details)
+};
+
+export type EditedSmartFormulaGoal = {
+  title: string; // this is the goal title
+  objectives: EditedSmartFormulaObjective[];
+};
+
+async function _generateSmartFormula(clientId: number, domainId: number, levelId: number) {
   const response = await api.post<SmartFormulaResponse>(`ai/smart-formula/${domainId}/${levelId}`);
   return response.data?.goals;
 }
 
+async function _saveSmartFormula(
+  clientId: number,
+  domainId: number,
+  levelId: number,
+  payload: { goals: EditedSmartFormulaGoal[] }
+) {
+  const response = await api.post(
+    `/clients/${clientId}/smart-formula/${domainId}/${levelId}/add`,
+    payload
+  );
+  return response.data;
+}
+
+async function _getSmartFormula(clientId: number, domainId: number, levelId: number) {
+  const response = await api.get<EditedSmartFormulaResponse>(
+    `/clients/${clientId}/smart-formula/${domainId}/${levelId}`
+  );
+  return response.data?.goals;
+}
+
 export function useSmartFormula(clientId: number, domainId: number, levelId: number) {
+  const queryClient = useQueryClient();
+
+  const generateSmartFormula = async () => await _generateSmartFormula(clientId, domainId, levelId);
+
+  const saveSmartFormula = async (payload: { goals: EditedSmartFormulaGoal[] }) => {
+    await _saveSmartFormula(clientId, domainId, levelId, payload),
+      queryClient.invalidateQueries(["smart-formula", clientId, domainId, levelId]);
+  };
+
+  const getSmartFormula = async () => await _getSmartFormula(clientId, domainId, levelId);
+
   return {
-    generateSmartFormula: async () => await getSmartFormula(clientId, domainId, levelId),
+    generateSmartFormula,
+    saveSmartFormula,
+    getSmartFormula,
   };
 }
+
+// Get edited smart formula for a specific client, domain and level
+export function useGetSmartFormula(clientId: number, domainId: number, levelId: number) {
+  return useQuery(["smart-formula", clientId, domainId, levelId], () =>
+    _getSmartFormula(clientId, domainId, levelId)
+  );
+}
+
+// async function getAllClientSmartFormula(clientId: number) {
+//   const response = await api.get<EditedSmartFormulaResponse>(`/clients/${clientId}/smart-formula`);
+//   return response.data?.goals;
+// }
+
+// // Gel all editied Goals and objectives
+// export function useClientSmartFormula(clientId: number) {
+//   return useQuery([clientId, "all-smart-formulas"], () => getAllClientSmartFormula(clientId));
+// }
