6ef871cc41e23c8c4edf91fbde23c1edf65bb005
elhoussin <razzoukihoussin@gmail.com>
2024-07-15 12:12:57 +0100

    add print pdf feature fro data sharing statement

--- app/(pages)/clients/[clientId]/questionnaire/data-sharing/page.tsx
+++ app/(pages)/clients/[clientId]/questionnaire/data-sharing/page.tsx
@@ -8,6 +8,7 @@ import { getDangerActionConfirmationModal } from "@/components/Modals/DangerActi
 import PaginatedTable from "@/components/PaginatedTable";
 import Panel from "@/components/Panel";
 import { useModal } from "@/components/providers/ModalProvider";
+import QuestionnaireDownloadButton from "@/components/QuestionnaireDownloadButton";
 import { DataSharingType } from "@/types/questionnaire/data-sharing";
 import { useDeleteDataSharing } from "@/utils/questionnairs/data-sharing/useDeleteDataSharing";
 import { useGetAllDataSharing } from "@/utils/questionnairs/data-sharing/useGetAllDataSharing";
@@ -71,7 +72,7 @@ const DataSharing: FunctionComponent<Props> = ({ params: { clientId } }) => {
                   <PencilSquare className="w-5 h-5" />
                 </IconButton>
               </Link>
-
+              <QuestionnaireDownloadButton type="data_sharing_statement" questId={+info.row.id} />
               <IconButton
                 className="bg-red"
                 onClick={() => {
