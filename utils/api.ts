81a1e5cfb75df5feac57278bd82a5d0de3206412
tf200 <farjiataha@gmail.com>
2024-12-26 19:06:12 +0100

    innit

--- utils/api.ts
+++ utils/api.ts
@@ -9,7 +9,7 @@ function authRequestInterceptor(config: InternalAxiosRequestConfig) {
 }
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "http://127.0.0.1:8000",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
